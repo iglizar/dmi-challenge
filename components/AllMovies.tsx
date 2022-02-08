10a85699fd97c6714ec444942f1c9a6218730d01
Ignacio Lizarraga <ilizarraga.dev@gmail.com>
2022-02-08 16:43:07 -0300

    Default value for movies

--- components/AllMovies.tsx
+++ components/AllMovies.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import EachThumbnail from "@/components/thumbnails/MovieThumbnail";
 import { IMovie } from "@/interfaces/MoviesInterface";
 
-const AllMovies = ({ movies }: { movies: IMovie[] }) => {
+const AllMovies = ({ movies=[] }: { movies: IMovie[] }) => {
   return (
     <div className="flex text-white justify-around flex-wrap grid grid-cols-2 md:grid-cols-5 xl:grid-cols-6 gap-x-4 gap-y-11">
       {movies.map((movie: IMovie, i: number) => (
